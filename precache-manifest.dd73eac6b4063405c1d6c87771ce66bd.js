daae54c5a8de381a395a5ee0c13dfc7b22886564
syntaxerror028 <zhellprodigalidad@gmail.com>
2020-04-10 17:49:02 +0800

    myPortfolio

---
+++ precache-manifest.dd73eac6b4063405c1d6c87771ce66bd.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a041a25ad918879acd0",
+    "url": "/css/about.67166143.css"
+  },
+  {
+    "revision": "07a71609c808e6b0a610",
+    "url": "/css/app.887f6b98.css"
+  },
+  {
+    "revision": "3b220dc744978cb7fce7",
+    "url": "/css/chunk-vendors.5b4f0ade.css"
+  },
+  {
+    "revision": "fc1d30af0979dcf7d222e4832f1f5a70",
+    "url": "/img/frontend.png"
+  },
+  {
+    "revision": "8013e7225a15224e72374c501e307172",
+    "url": "/img/my-avatar.8013e722.jpg"
+  },
+  {
+    "revision": "8fb3b55c67300d62e6b681b192ab6a08",
+    "url": "/img/smoke3.8fb3b55c.jpg"
+  },
+  {
+    "revision": "e224269f4602e0b7ff8eb3ac82f2a88a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a041a25ad918879acd0",
+    "url": "/js/about.f4a35433.js"
+  },
+  {
+    "revision": "07a71609c808e6b0a610",
+    "url": "/js/app.568b29c1.js"
+  },
+  {
+    "revision": "3b220dc744978cb7fce7",
+    "url": "/js/chunk-vendors.1ab9fe2f.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2a68f44b708e136cafcd266d9e51df7b",
+    "url": "/skills_img/android.png"
+  },
+  {
+    "revision": "c1ed916d4bb7736bc7f88312f206dbb9",
+    "url": "/skills_img/apigateway.png"
+  },
+  {
+    "revision": "c4b67fb5d5533aea56241b4c945fd7d0",
+    "url": "/skills_img/autodesk.png"
+  },
+  {
+    "revision": "c73ddeb1058502e9ece05fb8050b5c5b",
+    "url": "/skills_img/aws-reinvent-2016-workshop-secure-your-web-application-with-aws-waf-and-amazon-cloudfront-sac202-37-638.jpg"
+  },
+  {
+    "revision": "a9b726c4ea03dc3e07d6818fadf2b7a1",
+    "url": "/skills_img/aws.png"
+  },
+  {
+    "revision": "dce8b9af8a39a8957f58895e2feb1e76",
+    "url": "/skills_img/cloudfront.png"
+  },
+  {
+    "revision": "94bf0858977f29ff6013f84395478789",
+    "url": "/skills_img/couch.png"
+  },
+  {
+    "revision": "ddea356edecb00369301bb5736f93bc8",
+    "url": "/skills_img/couch2.png"
+  },
+  {
+    "revision": "07270c13412737933c6f3468d97618ad",
+    "url": "/skills_img/css3.png"
+  },
+  {
+    "revision": "941ded397a1dc02f04e3d8f32343331e",
+    "url": "/skills_img/dynamodb.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/skills_img/github.png"
+  },
+  {
+    "revision": "0ea316dbe0fd5dab21e160b3f48fa6b5",
+    "url": "/skills_img/html.png"
+  },
+  {
+    "revision": "3b93b35d538abf4b3a2f760ba16af86a",
+    "url": "/skills_img/iam.png"
+  },
+  {
+    "revision": "4eb1819dc863ef386e09d86e9f482a9f",
+    "url": "/skills_img/java.png"
+  },
+  {
+    "revision": "384d1e8c3c8dcfee10ab0b980f8075cd",
+    "url": "/skills_img/javascript.png"
+  },
+  {
+    "revision": "d8d94b886b329c6c5cd03446dc1f73e3",
+    "url": "/skills_img/lambda.png"
+  },
+  {
+    "revision": "819651106adfb135ab978eeeb154c817",
+    "url": "/skills_img/mysql.ico"
+  },
+  {
+    "revision": "c502658a509d27b53679b3ef73c0d82f",
+    "url": "/skills_img/nodejs.png"
+  },
+  {
+    "revision": "17d9ed2aa3da7a27035b0c5d5a6b2e03",
+    "url": "/skills_img/nodejs2.png"
+  },
+  {
+    "revision": "b187e3b1985b0aa738093d97ce028ab6",
+    "url": "/skills_img/php.png"
+  },
+  {
+    "revision": "a5526f892c40006910fd475d620b535a",
+    "url": "/skills_img/route53.jpg"
+  },
+  {
+    "revision": "67ef95501c69d7ea45126581ca659a0c",
+    "url": "/skills_img/route53.png"
+  },
+  {
+    "revision": "dfdd821d2cdf7a7d02f42a1bc2027cec",
+    "url": "/skills_img/s3bucket.png"
+  },
+  {
+    "revision": "93444c4f4c726658af3ea3dc4d745df2",
+    "url": "/skills_img/sns.png"
+  },
+  {
+    "revision": "b4c804a2999ec4e839a615dda8ebb512",
+    "url": "/skills_img/vuejs.png"
+  },
+  {
+    "revision": "624068b2c599b67259ac6479bf325e54",
+    "url": "/skills_img/vuetify.png"
+  }
+]);
